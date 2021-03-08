6e5cee4fa507dd768c588e3f51fae7aec9678c8c
shu279 <shusuke279@gmail.com>
2021-03-08 14:56:30 +0900

    ステージ選択画面の作成

---
+++ Assets/Scripts/StageIcon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageIcon : MonoBehaviour
+{
+    private bool iSelected;
+    public bool selected
+    {
+        get { return iSelected; }
+        set
+        {
+            if (value == true && iSelected == false)
+            {
+                GetComponent<Animator>().Play("Stage-icon Selected Animation");
+            }
+            iSelected = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
